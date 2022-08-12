f5bb919b61c50a74665bd72228f88e2f119205f7
Empajerek <konradjones@outlook.com>
2022-08-12 20:41:47 +0200

    v4

--- discordbot.py
+++ discordbot.py
@@ -1,19 +1,20 @@
+#!/usr/bin/env python3
+
 import os
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-notification_channel = "<@&779352944919183393>"
+notification_role = "<@&779352944919183393>"
 global StatMessage
 
 intents = discord.Intents.default()
 intents.members = True
-client = discord.Client(intents=intents)
-
-activeUsers = 0  
+bot = commands.Bot(command_prefix='KWA ',intents=intents)
 
 async def initUsersState(guild):
     global activeUsers
@@ -23,36 +24,35 @@ async def initUsersState(guild):
             print()
             activeUsers  += 1
 
-@client.event
+@bot.event
 async def on_ready():
-    for guild in client.guilds:
+    for guild in bot.guilds:
         if guild.name == GUILD:
             break
 
     print(
-        f'{client.user} is connected to the following guild:\n'
+        f'{bot.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
 
     await initUsersState(guild)
 
-    #members = '\n - '.join([member.name for member in guild.members])
-    #print(f'Guild Members:\n - {members}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user or message.author.bot == True:
+@bot.command(name="respond",help="responds with something.")
+@commands.has_role('botyk')
+async def respond(ctx):
+    if ctx.message.author == bot.user or ctx.message.author.bot == True:
         return
 
-    print(message)
-    if "mouthbreather" in message.content.split(" "):
+    if "mouthbreather" in ctx.message.content.split(" "):
         response = f"pls don't offend me."
-        await message.channel.send(response)
+        await ctx.send(response)
     else:
-        response = f"<@{message.author.id}> said {message.content}"
-        await message.channel.send(response)
+        response = f'<@{ctx.message.author.id}> said {"".join(ctx.message.content.split(" ")[2:])}'
+        await ctx.channel.send(response)
 
-@client.event
+
+#notification bot
+@bot.event
 async def on_voice_state_update(member, before, after):
     global activeUsers, notification_channel,StatMessage
     if before.channel is None and after.channel is not None:
@@ -61,9 +61,14 @@ async def on_voice_state_update(member, before, after):
         activeUsers -= 1
     for channel in member.guild.channels:
         if channel.name == 'tuba-nagłaśniająca':
-            try:
-                await StatMessage.edit(content=f"🎙️There are {activeUsers} people currently chatting!\n{notification_channel}")
-            except (NameError, discord.errors.NotFound):
-                StatMessage =  await channel.send(f"🎙️There are {activeUsers} people currently chatting!\n{notification_channel}")
-
-client.run(TOKEN)
+            if activeUsers:
+                try:
+                    await StatMessage.edit(content=f"🎙️There are {activeUsers} people currently chatting!\n{notification_role}")
+                except (NameError, discord.errors.NotFound):
+                    StatMessage =  await channel.send(f"🎙️There are {activeUsers} people currently chatting!\n{notification_role}")
+            else:
+                try:
+                    await StatMessage.delete()
+                except (NameError, discord.errors.NotFound):
+                    pass
+bot.run(TOKEN)
